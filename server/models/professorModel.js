b89bee50801221488dda9e65c8bb7e649a1ef950
Madhavan <madhavanvaidya@gmail.com>
2024-09-30 14:50:15 -0400

    Functionality Added: Professor Authentication

---
+++ server/models/professorModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const professorSchema = new mongoose.Schema(
+  {
+    professorID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const Professor = mongoose.model("professors", professorSchema);
+
+export default Professor;
